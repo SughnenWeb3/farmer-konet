e210b1175dc620da7c8937042466e17875c2afa9
sughnenweb3 <jforster460@gmail.com>
2026-02-18 08:03:08 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'script': ['Shadows Into Light', 'cursive'],
+      },
+    },
+  },
+}
